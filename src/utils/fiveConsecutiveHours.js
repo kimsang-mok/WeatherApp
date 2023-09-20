0b827d2ba0d0f64d045b50d3bfc102a3286b1314
kimsang-lov <kimsang.mok01@gmail.com>
2023-09-20 23:57:49 +0700

    add some change

--- src/utils/fiveConsecutiveHours.js
+++ src/utils/fiveConsecutiveHours.js
@@ -1,20 +1,4 @@
 export function extractFiveHoursForcast(data) {
-    const currentDate = new Date();
-    const currentHour = currentDate.getHours();
-    const numberOfHours = 5;
-    let forecast = [];
-    let startIndex = -1;
-
-    for (let i = 0; i < data.list.length; i++) {
-        const entry = data.list[i];
-        const dt = entry.dt;
-        const entryDate = new Date(dt * 1000);
-
-        if (entryDate.getHours() === currentHour + 1) {
-            startIndex = i;
-            break;
-        }
-    }
-    forecast = data.list.slice(startIndex, startIndex + numberOfHours);
+    const forecast = data.list.slice(0, 5)
     return forecast;
 }
